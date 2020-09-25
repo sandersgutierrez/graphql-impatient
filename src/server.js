ddbec2d6adf1f2f1c7384f012ea3eae62bb7aceb
Sanders Gutierrez <ing.sanders@gmail.com>
2020-09-25 04:36:26 -0300

    The basic structure of an API with GraphQL.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,25 @@
 'use strict'
 
-console.log('Hello World!')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query {
+        helloDominiCode: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        helloDominiCode() {
+            return 'Hola gente bella de DominiCode'
+        },
+    },
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+})
+
+server.listen(3030).then(() => console.log(`Server listening`))
