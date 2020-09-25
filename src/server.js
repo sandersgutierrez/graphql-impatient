317531173d4cae81032522f3acc62fc07c74450c
Sanders Gutierrez <ing.sanders@gmail.com>
2020-09-25 05:23:39 -0300

    Queries and Resolvers ok.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,45 @@
 'use strict'
 
+const fs = require('fs')
+const { join } = require('path')
 const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs = gql`
-    type Query {
-        helloDominiCode: String!
-    }
-`
+const schema = fs.readFileSync(join(__dirname, 'schema.gql'), 'utf8')
+const typeDefs = gql(schema)
+
+const users = [
+    {
+        name: 'Julian',
+        email: 'info@julianduque.co',
+        status: 'FAILED',
+    },
+    {
+        name: 'Sanders',
+        email: 'ing.sanders@gmail.com',
+        status: 'PASSED',
+    },
+]
+
+const courses = [
+    {
+        name: 'DominicCode 10K',
+        students: users,
+    },
+]
 
 const resolvers = {
     Query: {
         helloDominiCode() {
             return 'Hola gente bella de DominiCode'
+        },
+        listUsers(_, args) {
+            const { status } = args
+            if (!status) return users
+
+            return users.filter((u) => u.status === status)
+        },
+        listCourses() {
+            return courses
         },
     },
 }
